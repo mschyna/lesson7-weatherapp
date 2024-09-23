dd892316ed76f0e2950da31547dfd4f7a172e9f2
mschyna <syphon.ponder@gmail.com>
2024-09-23 17:17:29 -0400

    added icon

--- src/index.js
+++ src/index.js
@@ -7,7 +7,8 @@ let humidityElement = document.querySelector("#humidity");
 let windSpeedElement = document.querySelector("#wind-speed");
 let timeElement = document.querySelector("#time");
 let date = new Date(response.data.time * 1000 );
-let iconElement = document.querySelector("icon");
+let iconElement = document.querySelector("#icon");
+
 
 cityElement.innerHTML = response.data.city;
 timeElement.innerHTML = formatDate(date);
@@ -15,7 +16,7 @@ descriptionElement.innerHTML = response.data.condition.description;
 humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
 windSpeedElement.innerHTML = `${response.data.wind.speed}mi/h`;
 temperatureElement.innerHTML = Math.round(temperature);
-iconElement.innerHTML = `<img src="${repsonse.data.condition.icon_url}" class="weather-app-icon" />`;   
+iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;   
 }
 function formatDate(date) {
     
